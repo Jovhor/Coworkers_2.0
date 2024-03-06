e2cf9703df5efd8491b0860994d1d53780df32d3
Jovhor <dr.jovhor@mail.ru>
2024-03-06 17:48:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coworkers_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker();
+            worker.BirthDay = new DateTime(1999, 01, 11);
+
+            Console.WriteLine(worker.Print());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Coworkers_2._0
+{
+    struct Worker
+    {
+        /// <summary>
+        /// Номер по списку
+        /// </summary>
+        private int ID { get; set; }
+
+        /// <summary>
+        /// Время добавления сотрудника 
+        /// </summary>
+        private string AddTime { get { return AddTime; } set { AddTime = DateTime.Now.ToString(); } } 
+
+        /// <summary>
+        /// ФИО 
+        /// </summary>
+        public string FIO { get; set; }
+
+        /// <summary>
+        /// Возраст 
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Рост сотрудника
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Дата рождения сотрудника
+        /// </summary>
+        public DateTime BirthDay { get; set; } 
+
+        /// <summary>
+        /// Место рождения (place of birth) 
+        /// </summary>
+        public string PoB { get; set; } 
+
+        public string Print()  // вывод данных о сотруднике
+        {
+            return $"Номер по порядку: {ID} " +
+                $"\n Время добавления записи: {AddTime}" +
+                $"\nФ.И.О.: {FIO} " +
+                $"\nВозраст: {Age} " +
+                $"\nРост: {Height} " +
+                $"\nДата рождения: {BirthDay.ToShortDateString()}" +
+                $"\nМесто рождения: {PoB}";
+        }
+    }
+}
